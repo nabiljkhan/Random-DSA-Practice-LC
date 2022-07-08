b513e8a4c8736a8b600b845e58d0ae0d4f77efe0
Nabil Jahan Khan <83438134+nabiljkhan@users.noreply.github.com>
2022-07-08 19:45:35 +0530

    Add files via upload

---
+++ 50. Pow(x, n).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        while (n) {
+            if (n % 2) ans = n > 0 ? ans * x : ans / x;
+            x = x * x;
+            n /= 2;
+        }
+        return ans;
+    }
+};
+
+/* https://leetcode.com/problems/powx-n/ */
